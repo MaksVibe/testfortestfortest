a068bd9d8db6eae4d6d0a1bcef726be6036bf17d
MaksVibe <parunovmaksym@gmail.com>
2022-01-16 11:04:31 +0200

    sum

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import s from './App.module.scss';
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+
+  const handleChange = ({ target: { name, value } }) => {
+    switch (name) {
+      case 'email':
+        return setEmail(value);
+      case 'password':
+        return setPassword(value);
+      default:
+        break;
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={s.FormContainer}>
+      <form className={s.SignForm}>
+        <p>Вы можете авторизоваться с помощью Google Account:</p>
+        <a className={s.GoogleBtn}>Google</a>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Или зайти с помощью e-mail и пароля, предварительно
+          зарегистрировавшись:
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <label>Электронная почта:</label>
+        <input
+          type="email"
+          name="email"
+          value={email}
+          placeholder="your@email.com"
+          onChange={handleChange}
+          className={s.SignInput}
+        ></input>
+        <label>Пароль:</label>
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={handleChange}
+          className={s.SignInput}
+        ></input>
+        <ul className={s.SignBtnsWrap}>
+          <li>
+            <a className={s.SignInBtn}>войти</a>
+          </li>
+          <li>
+            <a className={s.SignUpBtn}>регистрация</a>
+          </li>
+        </ul>
+      </form>
     </div>
   );
 }
